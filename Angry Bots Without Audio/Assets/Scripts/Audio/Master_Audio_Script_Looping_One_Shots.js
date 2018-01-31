274ae9154e4de31ba845fd6fef274e4cffb6d85b
ChrisEverettAudioDesign <35993924+ChrisEverettAudioDesign@users.noreply.github.com>
2018-01-31 10:07:26 +0000

    commit

---
+++ Angry Bots Without Audio/Assets/Scripts/Audio/Master_Audio_Script_Looping_One_Shots.js
@@ -0,0 +1,53 @@
+﻿
+
+
+var MinPitchVariation = 0.9f;
+var MaxPitchVariation = 1.1f;
+
+var MinWaitTime : float	= 1.0;
+var MaxWaitTime : float = 2.0;
+
+var MinVolume : float	= 0.5;
+var MaxVolume : float	= 1.0;
+
+private var		timer : float	= 0.0;
+private var		nextInterval : float	= 1.0;
+private var 	audioSource : AudioSource	= null;
+
+var audioClips : AudioClip[];
+
+function Start () //Use the start function to initialise as it gets called when the game starts
+{
+
+	GetComponent.<AudioSource>().pitch = Random.Range(MinPitchVariation, MaxPitchVariation);//Random Pitch
+
+	nextInterval = Random.Range(MinWaitTime, MaxWaitTime);//Initialise nextInterval to a random interval
+	audioSource = GetComponent.<AudioSource>();//Get the AudioSource Component
+
+	//It's good practice to check things and throw errors
+
+	if(audioSource == null)
+		Debug.Log ("There is no audio source");
+
+	if(audioClips.Length == 0)	
+		Debug.LogError ("You haven't set any audio clips");
+}
+
+function Update () {
+
+	timer += Time.deltaTime;//delta time is time since last frame
+
+	if(timer >= nextInterval && !audioSource.IsPlaying)
+
+	{
+		audioSource.clip		= audioClips [Random.Range(0, audioClips.Length -1)]; // NOTE the Length -1 OTHERWISE you can go out of Range
+		audioSource.volume  	= Random.Range(MinVolume, MaxVolume); //Set Random Volume
+		audioSource.Play();											  //Play the Clip
+
+		nextInterval = Random.Range(MinWaitTime, MaxWaitTime); //Pick the time we wait until the next clip
+		timer = 0.0;										   //Zero the Timer
+
+
+		}
+
+}
---
+++ Angry Bots Without Audio/Assets/Scripts/Audio/Master_Audiosource.js
@@ -0,0 +1,40 @@
+﻿
+
+var SFX : AudioClip[];
+
+var MinPitchVariation = 0.9f;
+var MaxPitchVariation = 1.1f;
+
+var MinVolumeVariation = 0.4f;
+var MaxVolumeVariation = 0.6f;
+
+var ProbabilityOfSFXPlaying = 1f;
+
+function Update(){
+
+	//Press Spacebar to triggerSFX from array GET RID OF THIS! WHEN YOU ARE HAPPY THAT THE CODE IS WORKING!
+	if(Input.GetButtonUp("Jump"))
+
+	{
+
+		//Randomly select a sound from the Array to play
+		GetComponent.<AudioSource>().clip = SFX[Random.Range (0,SFX.Length -1)];
+
+		//Get Random Pitch
+		GetComponent.<AudioSource>().pitch = Random.Range (MinPitchVariation, MaxPitchVariation);
+
+		//Get Random Volume
+		GetComponent.<AudioSource>().volume = Random.Range (MinVolumeVariation, MaxVolumeVariation);
+
+		//Chance SFX will Play (Default to 100% chance)
+		if (Random.value <=ProbabilityOfSFXPlaying)
+
+		//Play SFX
+		GetComponent.<AudioSource>().Play();
+
+		//Else Don't Play
+		else;
+
+	}
+
+	}
