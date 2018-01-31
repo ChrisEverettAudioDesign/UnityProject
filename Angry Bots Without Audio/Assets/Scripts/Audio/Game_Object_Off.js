c3b42c83ffbcd903254fff7ce35d31544a7e7b85
ChrisEverettAudioDesign <35993924+ChrisEverettAudioDesign@users.noreply.github.com>
2018-01-31 16:11:19 +0000

    16:11 31/01/2018

---
+++ Angry Bots Without Audio/Assets/Scripts/Audio/Game_Object_Off.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var Game_Object : GameObject;
+
+function OnTriggerEnter(Trigger : Collider)
+
+{
+
+	if (Trigger.gameObject.name =="Player")
+	
+	Game_Object.SetActive(false);
+		Debug.Log("Player Has Turned Off Object");
+
+}
---
+++ Angry Bots Without Audio/Assets/Scripts/Audio/Game_Object_On.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var Game_Object : GameObject;
+
+function OnTriggerEnter(Trigger : Collider)
+
+{
+
+	if (Trigger.gameObject.name =="Player")
+		
+		Game_Object.SetActive(true);
+			Debug.Log("Player Has Turned Object On");
+}
